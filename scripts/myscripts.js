1ae098290fc3b9e3a4b0aa94d399e137227a8c2a
Unknown <roxydao97@gmail.com>
2017-12-07 01:57:46 +0200

    tab for movies slideshow made

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -4,31 +4,53 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();
 });
-$(document).ready(function(){
-    $(".inputfield").focus(function(){
-        $(".inputLabel").fadeTo(200,1).fadeIn(500).css("display", "block");
-    });
-});
-
-//sign in MODALS
+//sign in
 var signinButton = document.getElementById("sign-inButton");
 var signinModal = document.getElementById("sign-in");
-var closeButton = document.getElementsByClassName("close")[0];
 
 signinButton.onclick = function() {
   signinModal.style.display = "block";
 }
 
-closeButton.onclick = function() {
-  signinModal.style.display = "none";
-}
 
 window.onclick = function(event) {
-  if (event.target == signinModal) {
+  if ((event.target == signinModal)||(event.target == signupModal)){
     signinModal.style.display = "none";
+    signupModal.style.display = "none";
   }
 }
-
-function openModal() {
-  signinModal.style.display = "block";
+//sign up
+var signupButton = document.getElementById("sign-upButton");
+var signupModal = document.getElementById("sign-up");
+signupButton.onclick = function() {
+  signupModal.style.display = "block";
+  signinModal.style.display = "none";
 }
+//show password
+function showPass() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+//form
+$(document).ready(function(){
+    $(".inputfield").focus(function(){
+        $(".inputLabel").fadeTo(1000,1).fadeIn(500).css("display", "block");
+    });
+});
+
+//show and hide tabSlideshow
+$(document).ready(function(){
+  $(".tab-links a").on("click", function(e){
+    var currentTab = $(this).attr("href");
+    //show and hide tabs
+    $('.tab'+currentTab).show(600).siblings().hide(600);
+    //add active class
+    $(this).parent('li').addClass("active").siblings().removeClass("active");
+    //Prevent Default Anchor Link Behaviour
+    e.preventDefault();
+  });
+});
